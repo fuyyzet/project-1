29b77a0255151f3f7d8fc5b77e36fa71f130c056
Eyere AT <eyere@hotmail.com>
2024-02-01 15:06:48 -0500

    update

--- Jenkins-server/main.tf
+++ Jenkins-server/main.tf
@@ -7,6 +7,7 @@ module "vpc" {
 
   azs            = data.aws_availability_zones.available.names
   public_subnets = var.public_subnets
+  map_public_ip_on_launch = true
 
   enable_dns_hostnames = true
 
